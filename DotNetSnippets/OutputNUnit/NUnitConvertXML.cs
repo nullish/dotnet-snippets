e307700fa581af0f34a87885c4619797109a6952
Nullish <richerde43@gmail.com>
2024-12-10 14:57:11 +0000

    Parse error message and format

--- DotNetSnippets/OutputNUnit/NUnitConvertXML.cs
+++ DotNetSnippets/OutputNUnit/NUnitConvertXML.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -9,6 +10,13 @@ namespace OutputNUnit
 {
     public class NUnitConvertXML
     {
+        private string MultiToSingleLine(string inputString) {
+           // Receive a multi line string and remove new line chars. Trim and remove surplus characters.
+            string outputString = Regex.Replace(inputString, @"\n", " ");
+            outputString = Regex.Replace(outputString, @"-{2,}\^", "");
+            outputString = outputString.Trim();
+            return outputString;
+        }
         public void ConvertCSV(string pathInput, string pathOutput)
         /// <summary>Convert NUnit format XML into CSV, selected fields only </summary>
         /// <param name="pathInput">Relative path for input file</param>
@@ -23,12 +31,13 @@ namespace OutputNUnit
             string csv =
                 (from el in xmlFiltered.Elements("test-case")
                  select
-                     string.Format("{0}|{1}|{2}|{3}|{4}|{5}",
+                     string.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}",
                          (string)el.Attribute("name"),
                          (string)el.Attribute("fullname"),
                          (string)el.Element("properties").Elements("property")?.FirstOrDefault(p => p.Attribute("name")?.Value == "Category")?.Attribute("value") ?? "-",
                          (string)el.Attribute("start-time"),
                          (string)el.Attribute("result"),
+                         MultiToSingleLine((string)el?.Element("failure")?.Element("message") ?? "-"),
                          Environment.NewLine
                      )
                 )
